c606897c4a725ac61353ab41193a7620f98c59b9
Leonard Nzekwe <nzekweleo@gmail.com>
2024-01-03 21:35:03 +0100

    Fix ES6 Promise tasks{2,3,6,9}

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,6 @@
 export default function handleResponseFromAPI(promise) {
-  promise
-    .then(() => {
-      console.log('Got a response from the API');
-      return { status: 200, body: 'success' };
-    })
-    .catch(() => {
-      return Error();
-    });
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => (Error()))
+    .finally(() => (console.log('Got a response from the API')));
 }
