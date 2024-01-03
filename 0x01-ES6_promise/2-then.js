047efad90f7be9c661f78e36915b1e106328f822
Leonard Nzekwe <nzekweleo@gmail.com>
2024-01-03 21:14:02 +0100

    Update ES6 Promise task 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -5,7 +5,6 @@ export default function handleResponseFromAPI(promise) {
       return { status: 200, body: 'success' };
     })
     .catch(() => {
-      console.log('Got a response from the API');
-      return new Error();
+      return Error();
     });
 }
