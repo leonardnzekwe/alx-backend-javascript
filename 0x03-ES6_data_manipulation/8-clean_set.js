2fb57486fa0501f21d32b08303554fabd0e3b90f
Leonard Nzekwe <nzekweleo@gmail.com>
2024-01-09 00:07:04 +0100

    Update ES6 Data Manipulation task 8: style

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,6 @@
 export default function cleanSet(set, startString) {
   if (!startString || !set || !(set instanceof Set) || typeof startString !== 'string') return '';
-  
+
   return Array.from(set)
     .filter((val) => val && val.startsWith(startString))
     .map((val) => val.replace(startString, ''))
