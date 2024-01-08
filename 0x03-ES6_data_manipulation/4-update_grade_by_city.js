f271b99cdec2bc27c34953083a9f8aaa7a99c951
Leonard Nzekwe <nzekweleo@gmail.com>
2024-01-08 23:48:33 +0100

    Add ES6 Data Manipulation Tasks

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,17 @@
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
+      if (gradeObj) {
+        return {
+          ...student,
+          grade: gradeObj.grade,
+        };
+      }
+      return {
+        ...student,
+        grade: 'N/A',
+      };
+    });
+}
---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,5 @@
+export default function cleanSet(set, startString) {
+  const filteredValues = Array.from(set).filter((value) => value.startsWith(startString));
+  const cleanedString = filteredValues.map((value) => value.slice(startString.length)).join('-');
+  return cleanedString;
+}
