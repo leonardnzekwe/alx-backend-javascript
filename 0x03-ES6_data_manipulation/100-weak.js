bae726fc1d1546a5fd3594239911a1847bcc8d64
Leonard Nzekwe <nzekweleo@gmail.com>
2024-01-09 01:36:46 +0100

    Update ES6 Data Manipulation 100-weak.js

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,14 +1,11 @@
 const weakMap = new WeakMap();
 
 function queryAPI(endpoint) {
-  if (!weakMap.has(endpoint)) {
-    weakMap.set(endpoint, 1);
-  } else {
-    const count = weakMap.get(endpoint);
-    if (count >= 5) {
-      throw new Error('Endpoint load is high');
-    }
-    weakMap.set(endpoint, count + 1);
+  const count = weakMap.get(endpoint) || 0;
+  weakMap.set(endpoint, count + 1);
+
+  if (weakMap.get(endpoint) >= 5) {
+    throw new Error('Endpoint load is high');
   }
 }
 
